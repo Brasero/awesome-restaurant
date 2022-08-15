08e51a89e9c0a5272cfed11cd386a3d258af3fe1
brasero <brandonricci4@gmail.com>
2022-08-15 17:01:53 +0200

    base of style for ingredient page

--- public/dash/index.php
+++ public/dash/index.php
@@ -13,6 +13,7 @@ $bdd = new Database('exemple_panier', 'root', '', 'localhost');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/ingredient.css">
     <title>Burger Compagny</title>
 </head>
 <body>
--- vue/dash/ingredient.php
+++ vue/dash/ingredient.php
@@ -3,6 +3,16 @@
 
 ?>
 
-
-
-<h1>Gestion des ingrédients</h1>
+<div class="ingredientContainer">
+    <h1 class="ingredientTitle">
+        Gestion des ingrédients
+    </h1>
+    <div class="ingredientCardDeck">
+        <div class="card">Ajout ingrédient</div>
+        <div class="card">Ajout Type ingrédient</div>
+    </div>
+    <div class="ingredientCardDeck">
+        <div class="table"></div>
+        <div class="table"></div>
+    </div>
+</div>
