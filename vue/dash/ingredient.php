063b89e06cbbb01d41a38a0b069940ac3b00cf11
brasero <brandonricci4@gmail.com>
2022-08-15 22:45:08 +0200

    ajout de type ingredient & affichage en liste déroulante dans l ajout d ingredient

--- vue/dash/ingredient.php
+++ vue/dash/ingredient.php
@@ -1,5 +1,20 @@
 <?php
 
+//require du controller de la page
+require_once('../../controller/dash/ingredientController.php');
+
+
+//Soummission du formulaire ajout de type d'ingrédient
+if(isset($_POST['ingredientTypeNom'])){
+    echo addTypeIngredient($bdd->connection, $_POST['ingredientTypeNom']);
+}
+
+//Soummission du formulaire ajout d'ingrédient
+if(isset($_POST['ingredientNom'], $_POST['ingredientType'])){
+
+}
+
+$types = getAllType($bdd->connection);
 
 ?>
 
@@ -8,9 +23,8 @@
         Gestion des ingrédients
     </h1>
     <div class="ingredientCardDeck">
-        <div class="card">Ajout ingrédient</div>
         <div class="card">
-            <h4 class="formTitle">
+        <h4 class="formTitle">
                 Ajouter un ingrédient
             </h4>
             <form action="" method="post">
@@ -21,6 +35,39 @@
                         <span>Nom</span>
                     </label>
                 </div>
+                <div class="typeChoice">
+                    <label for="ingredientType">
+                        <span>Type d'ingrédient</span>
+                    </label>
+                    <select 
+                    class="inputItem" name="ingredientType"
+                    id="ingredientType" placeholder="Type d'ingrédient" default="false">
+                        <option class="typeOption" value="false">....</option>
+                        <?php foreach ($types as $type){ ?>
+                            <option value="<?= $type['id'] ?>"><?= $type['nom'] ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <button type="submit" class="addButton">
+                    Ajouter
+                </button>
+            </form>
+        </div>
+        <div class="card">
+            <h4 class="formTitle">
+                Ajouter type d'ingrédient
+            </h4>
+            <form action="" method="post">
+                <div class="inputGroup">
+                    <label for="ingredientTypeNom" class="inputLabel">
+                        <input type="text" class="inputItem" name="ingredientTypeNom"
+                        id="ingredientTypeNom" placeholder="Nom" />
+                        <span>Nom</span>
+                    </label>
+                </div>
+                <button type="submit" class="addButton">
+                    Ajouter
+                </button>
             </form>
         </div>
     </div>
