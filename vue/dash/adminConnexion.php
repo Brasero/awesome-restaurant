7a6b6093e5ba828fe9a14b79af37bb6e1fe3fca5
brasero <brandonricci4@gmail.com>
2022-08-20 14:10:44 +0200

    styling connexion admin page

--- vue/dash/adminConnexion.php
+++ vue/dash/adminConnexion.php
@@ -10,28 +10,31 @@ if(isset($_POST['mail'], $_POST['mdp'])){
 
 
 <div class="connexionContainer">
+  <div class="formContainer">
     <h1 class="formTitle">
-        Administration Burger Compagny
+        <div class="top">Administration</div>
+        <div class="bottom"><span class="left">Burger</span> <span class="right">Compagny</span></div>
     </h1>
-  <form action="" method="POST" class="loginForm">
-    <div class="inputGroup">
-      <label for="mail" class="inputLabel">
-      <input type="email" name="mail" id="mail" placeholder="Email" class="signInput">
-        <span>Email</span>
-      </label>
-    </div>
+    <form action="" method="POST" class="loginForm">
+      <div class="inputGroup">
+        <label for="mail" class="inputLabel">
+        <input type="email" name="mail" id="mail" placeholder="Email" class="signInput">
+          <span>Email</span>
+        </label>
+      </div>
 
-    <div class="inputGroup">
-      <label for="mdp" class="inputLabel">
-        <input type="password" name="mdp" placeholder="Mot de passe" id="mdp" class="signInput">
-        <span>Mot de passe</span>
-      </label>
-    </div>
-    
-    
-    
-    <button type="submit" class="loginButton">
-      Connexion
-    </button>
-  </form>
+      <div class="inputGroup">
+        <label for="mdp" class="inputLabel">
+          <input type="password" name="mdp" placeholder="Mot de passe" id="mdp" class="signInput">
+          <span>Mot de passe</span>
+        </label>
+      </div>
+      
+      
+      
+      <button type="submit" class="loginButton">
+        Connexion
+      </button>
+    </form>
+  </div>
 </div>
