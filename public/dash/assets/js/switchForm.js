dcea07a9904eadedfe35907e05631d3ddcdd7556
brasero <brandonricci4@gmail.com>
2022-08-22 14:03:02 +0200

    adding text-shadow to back button, changing root css variable name en alphabetique sort, testing theme color on client side, adding favIcon

--- public/dash/assets/js/switchForm.js
+++ public/dash/assets/js/switchForm.js
@@ -2,7 +2,7 @@ function switchForm(to){
     var firstPart = document.querySelector('.produitFormPart1')
     var secondPart = document.querySelector('.produitFormPart2')
     if(to == 'toLeft'){
-        firstPart.setAttribute('style', 'transform: translateX(-100%)')
+        firstPart.setAttribute('style', 'transform: translateX(-110%)')
         secondPart.setAttribute('style', 'transform: translateX(-100%)')
     }else{
         firstPart.setAttribute('style', 'transform: translateX(0)')
--- public/index.php
+++ public/index.php
@@ -12,6 +12,7 @@ var_dump($_SESSION);
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./assets/icon/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="./assets/css/style.css">
   <title>Burger Compagny</title>
 </head>
