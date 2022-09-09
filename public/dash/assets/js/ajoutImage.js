0d85b0a1515a2424c3fae464afe83532614c94d2
RightClicke <aminedrici1@gmail.com>
2022-09-09 15:19:28 +0200

    js ajout photo

--- public/dash/assets/js/ajoutImage.js
+++ public/dash/assets/js/ajoutImage.js
@@ -3,7 +3,7 @@ var image =document.querySelector('#image')
 var inputVisible = document.querySelector('#fakeInput')
  if(image.value){
     var files = event.target.files
-      for (const file of files) {
+      for (var file of files) {
        inputVisible.innerHTML =file.name + ' a été choisie.'
 
   }
