36943243cea4a0f013a50069106cf9f109736483
RightClicke <aminedrici1@gmail.com>
2022-09-15 15:33:47 +0200

    responsive+correction

--- vue/carte.php
+++ vue/carte.php
@@ -6,15 +6,14 @@ if (isset($_GET['addItem'])) {
 }
 ?>
 
-<!-- titre + selecteur de categorie -->
-<div class="cartContainer">
+<div class="carteContainer">
   <div class="selectCarte">
     <h1>Notre carte</h1>
      <select name="" id="selectCat">
       <option value="">burger</option>
     </select>
   </div>
-
+  
 <!-- debut boucle pour boucle categorie -->
   <div class="cardDeck">
     <h1>Categorie</h1>
@@ -28,29 +27,118 @@ if (isset($_GET['addItem'])) {
       </div>
       <div class="cardButtonGroup">
         <button class="cardButtonAdd">
-         <iconify-icon id="cartIcone" icon="noto-v1:shopping-cart"></iconify-icon>
+          +Ajouter 
         </button>
         <button class="cardButtonAdd">
-          +Ajouter 
+         <iconify-icon id="cartIcone" icon="noto-v1:shopping-cart"></iconify-icon>
         </button>
       </div>
     </div> 
+
+      <div class="cardProd">
+      <img src="../public/assets/img/ressources/background/carte/test.png" alt="">
+      <div> 
+        <h2>Le burger 1</h2>
+        <p>La liste des ingredient</p>
+      </div>
+      <div class="cardButtonGroup">
+        <button class="cardButtonAdd">
+          +Ajouter 
+        </button>
+        <button class="cardButtonAdd">
+         <iconify-icon id="cartIcone" icon="noto-v1:shopping-cart"></iconify-icon>
+        </button>
+      </div>
+    </div>   <div class="cardProd">
+      <img src="../public/assets/img/ressources/background/carte/test.png" alt="">
+      <div> 
+        <h2>Le burger 1</h2>
+        <p>La liste des ingredient</p>
+      </div>
+      <div class="cardButtonGroup">
+        <button class="cardButtonAdd">
+          +Ajouter 
+        </button>
+        <button class="cardButtonAdd">
+         <iconify-icon id="cartIcone" icon="noto-v1:shopping-cart"></iconify-icon>
+        </button>
+      </div>
+    </div>   <div class="cardProd">
+      <img src="../public/assets/img/ressources/background/carte/test.png" alt="">
+      <div> 
+        <h2>Le burger 1</h2>
+        <p>La liste des ingredient</p>
+      </div>
+      <div class="cardButtonGroup">
+        <button class="cardButtonAdd">
+          +Ajouter 
+        </button>
+        <button class="cardButtonAdd">
+         <iconify-icon id="cartIcone" icon="noto-v1:shopping-cart"></iconify-icon>
+        </button>
+      </div>
+    </div>   <div class="cardProd">
+      <img src="../public/assets/img/ressources/background/carte/test.png" alt="">
+      <div> 
+        <h2>Le burger 1</h2>
+        <p>La liste des ingredient</p>
+      </div>
+      <div class="cardButtonGroup">
+        <button class="cardButtonAdd">
+          +Ajouter 
+        </button>
+        <button class="cardButtonAdd">
+         <iconify-icon id="cartIcone" icon="noto-v1:shopping-cart"></iconify-icon>
+        </button>
+      </div>
+    </div> 
+
   </div>
-<!-- fin de boucle categorie et produit  -->
+</div>
 
 <!-- debut panier -->
 
-  <div class="cardPanier">
+</div>
+<div class="panierContainer">
+    <div class="cardPanier">
     <h1>Mon panier</h1>
     <span></span>
 <!-- debut boucle produit dans panier -->
-    <div class="panierProd">
-      <img src="../public/assets/img/ressources/background/carte/test.png" alt="" class="imageProdPanier">
-      <p class="nomProduit">Nom produit</p>
-      <p class="prixProduit">10.50</p>
-      <span></span>
+    <div class="containerPanierProd">
+      <div class="panierProd">
+        <img src="../public/assets/img/ressources/background/carte/test.png" alt="" class="imageProdPanier">
+        <p class="nomProduit">Nom produit</p>
+        <p class="prixProduit">10.50</p>
+        <span></span>
+      </div>  
+
+      <div class="panierProd">
+        <img src="../public/assets/img/ressources/background/carte/test.png" alt="" class="imageProdPanier">
+        <p class="nomProduit">Nom produit</p>
+        <p class="prixProduit">10.50</p>
+        <span></span>
+      </div>  <div class="panierProd">
+        <img src="../public/assets/img/ressources/background/carte/test.png" alt="" class="imageProdPanier">
+        <p class="nomProduit">Nom produit</p>
+        <p class="prixProduit">10.50</p>
+        <span></span>
+      </div>  <div class="panierProd">
+        <img src="../public/assets/img/ressources/background/carte/test.png" alt="" class="imageProdPanier">
+        <p class="nomProduit">Nom produit</p>
+        <p class="prixProduit">10.50</p>
+        <span></span>
+      </div>  <div class="panierProd">
+        <img src="../public/assets/img/ressources/background/carte/test.png" alt="" class="imageProdPanier">
+        <p class="nomProduit">Nom produit</p>
+        <p class="prixProduit">10.50</p>
+        <span></span>
+      </div>  <div class="panierProd">
+        <img src="../public/assets/img/ressources/background/carte/test.png" alt="" class="imageProdPanier">
+        <p class="nomProduit">Nom produit</p>
+        <p class="prixProduit">10.50</p>
+        <span></span>
+      </div>  
 <!-- fin de boucle --> 
-    </div>
     <div class="cardTotal">
       <span id="spanTotal"></span>
       <p class="textTotal">Total:</p>
@@ -59,10 +147,12 @@ if (isset($_GET['addItem'])) {
   </div>
 
 <!-- fin panier -->
-<div class="panierToggle" onclick="togglePanier()">
-  <div class="top"></div>
-  <div class="bottom"></div>
-</div>
+    <div class="panierToggle" onclick="togglePanier()">
+      <div class="top"></div>
+      <div class="bottom"></div>
+      <div class="mid"></div>
+    </div>
+  </div>
 
 </div>
 
