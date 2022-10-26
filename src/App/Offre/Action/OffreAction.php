54ea5eccf8249d14adc85a7f86cbd0ab66190950
RightClicke <aminedrici1@gmail.com>
2022-10-26 14:20:35 +0200

    correction fail

--- src/App/Offre/Action/OffreAction.php
+++ src/App/Offre/Action/OffreAction.php
@@ -59,7 +59,7 @@ class OffreAction
 
         // Validation des données
         $validator = new Validator($data);
-        $validator->required( 'taux')
+        $validator->required( 'nom', 'taux', 'date_debut', 'date_fin')
             ->strLength('nom', 2, 255)
             ->strSize('nom', 2, 100)
             ->intLength('taux', 0, 100)
