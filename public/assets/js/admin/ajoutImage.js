2475eb3d1e252802b800e9b088f892fcca45ee96
RightClicke <aminedrici1@gmail.com>
2022-10-28 00:05:19 +0200

    update

--- public/assets/js/admin/ajoutImage.js
+++ public/assets/js/admin/ajoutImage.js
@@ -1,11 +1,23 @@
-function addFileText(event){
-var image =document.querySelector('#image')
-var inputVisible = document.querySelector('#fakeInput')
- if(image.value){
-    var files = event.target.files
-      for (var file of files) {
-       inputVisible.innerHTML =file.name + ' a été choisie.'
+function addFileText(event) {
+    var image = document.querySelector('#image')
+    var inputVisible = document.querySelector('#fakeInput')
+    if (image.value) {
+        var files = event.target.files
+        for (var file of files) {
+            inputVisible.innerHTML = file.name + ' a été choisie.'
 
-  }
+        }
+    }
 }
+
+function addFileTextupdate(event) {
+    var image = document.querySelector('#imageupdate')
+    var inputVisible = document.querySelector('#fakeInputupdate')
+    if (image.value) {
+        var files = event.target.files
+        for (var file of files) {
+            inputVisible.innerHTML = file.name + ' a été choisie.'
+
+        }
+    }
 }
