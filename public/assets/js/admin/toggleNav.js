43d962017f8c97deed5ad6b4b7005b7dade3258a
Redsnak57 <corentin.malbrun1@gmail.com>
2022-11-09 10:31:23 +0100

    premier try nav

--- public/assets/js/admin/toggleNav.js
+++ public/assets/js/admin/toggleNav.js
@@ -8,3 +8,11 @@ function toggleNav(){
     toggleButton.classList.toggle('active')
 }
 
+// let hamburger = document.querySelector('.navBurger');
+// let navBar = document.querySelector('.navBar');
+// let navLinks = document.querySelector('.navLinks');
+
+// hamburger.addEventListener('click', () => {
+//     navBar.classList.toggle("active");
+//     navLinks.classList.toggle("active");
+// })
