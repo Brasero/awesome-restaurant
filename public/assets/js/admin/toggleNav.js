445ad6e6b6a3ce9ca62c8add1a976c0704511492
Redsnak57 <corentin.malbrun1@gmail.com>
2022-11-09 11:54:50 +0100

    PremierVNav + factorise css + resp user

--- public/assets/js/admin/toggleNav.js
+++ public/assets/js/admin/toggleNav.js
@@ -8,11 +8,19 @@ function toggleNav(){
     toggleButton.classList.toggle('active')
 }
 
-// let hamburger = document.querySelector('.navBurger');
-// let navBar = document.querySelector('.navBar');
-// let navLinks = document.querySelector('.navLinks');
+// responsive nav  
+let hamburger = document.querySelector('.navBurger');
+let navBar = document.querySelector('.navBar');
+let navLinks = document.querySelector('.navLinks');
+let closeNav = document.querySelector(".close");
 
-// hamburger.addEventListener('click', () => {
-//     navBar.classList.toggle("active");
-//     navLinks.classList.toggle("active");
-// })
+hamburger.addEventListener('click', () => {
+    navBar.classList.toggle("active");
+    navLinks.classList.toggle("active");
+    if(navBar.classList.contains("navBar")){
+        closeNav.classList.add("active");
+    }
+    if(navBar.classList.contains("active")){
+        closeNav.classList.remove("active");
+    }
+})
