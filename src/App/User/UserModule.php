6ba75fddd56b91363200f452376050e98ec4b0ec
RightClicke <aminedrici1@gmail.com>
2022-11-15 10:20:00 +0100

    design page adresse et page parametre profil user

--- src/App/User/UserModule.php
+++ src/App/User/UserModule.php
@@ -43,6 +43,7 @@ class UserModule extends Module
 
         $router->get($prefix . '/espace', [$this, 'espace'], 'user.espace');
         $router->get($prefix . '/paramProfil', [$this, 'paramProfil'], 'user.paramProfil');
+        $router->get($prefix . '/adresse', [$this, 'adresse'], 'user.adresse');
 
         if ($renderer instanceof TwigRenderer) {
             $renderer->getTwig()->addExtension($menuTwigExtension);
@@ -77,6 +78,11 @@ class UserModule extends Module
     public function paramProfil(): string
     {
         return $this->renderer->render('@user/paramProfil');
+    }
+
+    public function adresse(): string
+    {
+        return $this->renderer->render('@user/adresse');
     }
 
     public function show()
