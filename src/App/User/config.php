f13000154beedeca0efe20a0e0a51306ae8b40fe
RightClicke <aminedrici1@gmail.com>
2022-12-02 15:48:05 +0100

    correction bug

--- src/App/User/config.php
+++ src/App/User/config.php
@@ -12,10 +12,11 @@ return [
         \DI\get(UserWidget::class)
     ]),
     "user.widgets" => \DI\add([
-        \DI\get(\App\User\ParamProfilWidget::class),
-        \DI\get(\App\User\AdresseWidget::class),
-        \DI\get(\App\User\AideWidget::class),
         \DI\get(\App\User\CommandeWidget::class),
+        \DI\get(\App\User\AdresseWidget::class),
+        \DI\get(\App\User\ParamProfilWidget::class),
+        \DI\get(\App\User\AideWidget::class),
+
     ]),
     UserModule::class => autowire()->constructorParameter('prefix', get('user.prefix')),
     MenuTwigExtension::class => \DI\autowire()->constructorParameter('widgets', \DI\get('admin.widgets')),
